226d1536029cad7c812bea76d68ee84c85007377
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2021-08-03 11:52:49 -0500

    Completed string exercise

--- src/Bob.java
+++ src/Bob.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 public class Bob {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        boolean userTalk = true;
+
+        do {
+            System.out.println("Why not try talking to Bob?");
+            String userString = scanner.nextLine();
+            if (userString.endsWith("?")) {
+                System.out.println("Sure.");
+            } else if (userString.endsWith("!")){
+                System.out.println("Whoa, chill out!");
+            } else if (userString.endsWith(" ")){
+                System.out.println("Fine. Be that way!");
+            } else {
+                System.out.println("Whatever.");
+            }
+        }while(userTalk);
     }
 }
