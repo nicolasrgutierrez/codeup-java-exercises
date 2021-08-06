fd60c37ac8850dcf32596ac20c4a2b490b0232df
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2021-08-06 16:13:31 -0500

    Finished override

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,13 +1,19 @@
 package shapes;
 
 public class Square extends Rectangle{
-
+    private double side;
     public Square(double side) {
         super(side, side);
+        this.side = side;
     }
 
     @Override
     public double getArea() {
-        return super.getArea();
+        return side * side;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return side * 4;
     }
 }
