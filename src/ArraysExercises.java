f8dcb7234772c46c1d8303ce7cfa1aaa9b415695
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2021-08-10 13:52:50 -0500

    Walkthrough of addPerson

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class ArraysExercises {
 
     public static void main(String[] args) {
@@ -7,14 +9,25 @@ public class ArraysExercises {
 
         Person[] people = new Person[3];
 
-        people[0] = new Person("Mike");
-        people[1] = new Person("Scott");
-        people[2] = new Person("Trevor");
+        people[0] = new Person("Midoriya");
+        people[1] = new Person("Bakugo");
+        people[2] = new Person("Todoroki");
+
+        for(int i = 0; i < people.length; i++) {
+            System.out.println(people[i].getName());
+        }
+
+        people = addPerson(people, new Person("Iida"));
 
         for(Person person : people) {
             System.out.println(person.getName());
         }
-
     }
+        public static Person[] addPerson(Person[] people, Person personToAdd) {
+            Person[] newPeopleArray = Arrays.copyOf(people, people.length + 1);
+            newPeopleArray[newPeopleArray.length - 1] = personToAdd;
+
+            return newPeopleArray;
+        }
 
 }
